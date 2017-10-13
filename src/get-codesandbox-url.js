1f123b030d85f3fd6fe9dd4ab1e5786824c88f5e
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-10-13 16:18:46 +0200

    Support Netlify builds

--- src/get-codesandbox-url.js
+++ src/get-codesandbox-url.js
@@ -2,8 +2,27 @@ const branch = require('git-branch');
 const username = require('git-username');
 
 export default function getSandboxUrl(path) {
-  const currentBranch = branch.sync();
-  const currentUsername = username();
+  let currentBranch;
+  let currentUsername;
+
+  // Check whether the build is happening on Netlify
+  if (process.env.REPOSITORY_URL) {
+    const usernameParts = process.env.REPOSITORY_URL.match(
+      /github.com\/(.*)\/reactjs\.org/,
+    );
+    currentUsername = usernameParts[1];
+    currentBranch = process.env.BRANCH;
+  } else {
+    currentBranch = branch.sync();
+    currentUsername = username();
+  }
+
+  if (!currentBranch) {
+    throw new Error('Could not fetch branch from the git info.');
+  }
+  if (!currentUsername) {
+    throw new Error('Could not fetch username from the git info.');
+  }
 
   return `https://codesandbox.io/embed/github/${currentUsername}/reactjs.org/tree/${currentBranch}/${path}?codemirror=1&hidenavigation=1`;
 }
